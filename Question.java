0cdb022c51cbe0e035783d38748b5395bee936c0
Nayeem Ahammed <nayeem.ahammed01@gmail.com>
2025-04-25 20:16:02 +0600

    set and get User name, password and role of User.java

--- Question.java
+++ Question.java
@@ -1,3 +1,28 @@
+/*
+ * 
+ */
+import java.util.List;
+
 public class Question {
+    private String questionText;
+    private List<String> options;
+    private int correctAnswerIndex;
+
+    public Question(String questionText, List<String> options, int correctAnswerIndex) {
+        this.questionText = questionText;
+        this.options = options;
+        this.correctAnswerIndex = correctAnswerIndex;
+    }
+
+    // getters
+    public String getQuestion() {
+        return questionText;
+    }
+    public List<String> getOptions() {
+        return options;
+    }
+    public int getCorrectAnswerIndex() {
+        return correctAnswerIndex;
+    }
     
 }
--- User.java
+++ User.java
@@ -1,3 +1,24 @@
+/*
+ *  find user name, password and role
+ */
 public class User {
+    String username, password, role; // role = "admin", "teacher", "student"
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    // getters & sutter
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getRole() {
+        return role;
+    }
     
 }
